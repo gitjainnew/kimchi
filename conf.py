ddbcd822534010784fbea4b949a718eb0f4841f0
gitjainnew <gitjain09@gmail.com>
2025-07-22 01:15:59 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -10,8 +10,7 @@ project = ''
 
 html_meta = {
     "msvalidate.01": "BFF40CA8D143BAFDF58796E4E025829B"
-    "title": "mochi Health:know everything beofre subscription"
-    "description": "how Mochi Health is helping Americans manage obesity through affordable weight loss medications, virtual care, and insurance support"
+    
   
    
 }
